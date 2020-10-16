d584bd8585f2a052708cbeaeb03dd785c4ce6cfe
Study <study@Dogas-MacBook-Pro.local>
2020-10-16 22:15:00 +0300

    tasks

---
+++ scripts/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#!/bin/bash
+echo -e "Hello there! How do you feel about bash scripting?"
+read
+echo "You said $REPLY. Lab exercise 1 is done! Thank you!"
+echo $(uname -a) >> ../doc/info/info.txt
